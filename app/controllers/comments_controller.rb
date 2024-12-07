dc0c9285f7a82b6787a40e9b081bd8b7e78fd3b8
Daniel Park <intheimageofyeshua@gmail.com>
2024-12-07 18:38:08 +0900

    Comment Destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,15 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
   private
     def comment_params
-      params.expect(comment: [:commenter, :body])
+      params.expect(comment: [:commenter, :body, :status])
     end
 end
